62a3a07b31c0a1dcbadc106748465b5f45e7698c
laura Torres <laura09torres05@gmail.com>
2026-09-22 12:28:33 -0500

    solucion de tarea arrays

---
+++ sd-l2-1.0/task7.js
@@ -0,0 +1,26 @@
+const arr = [
+    [0,1,2,3,4,5,6,7,8,9],
+    [10,11,12,13,14,15,16,17,18,19],
+    [20,21,22,23,24,25,26,27,28,29]
+  ]
+  
+  // ¡Escribe tu código debajo de esta línea!
+    //agregar un numero a lila existente 
+    arr[1].push(20);
+
+
+
+
+    //gregar una fila completa
+    arr.push([30,31,32,33,34,35,36,37,38,39]);  
+
+    
+
+    //eliminar  un solo nuemro de una solsa fila 
+    arr[0].splice(5,1);
+
+
+
+  
+  
+  // ¡Escribe tu código encima de esta línea!
